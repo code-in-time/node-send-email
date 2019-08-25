0a6eb28bca954c9b58802995267bf7a30cfd5500
code-in-time@hotmail.com <andrew.crawford.za@gmail.com>
2019-08-25 21:51:49 +0200

    chnage send gris details

--- index.js
+++ index.js
@@ -11,20 +11,16 @@ app.get("/url", (req, res, next) => {
 
 // https://app.sendgrid.com/guide/integrate/langs/nodejs
 app.post('/sendMail', function (req, res) {
-
-
-    // using SendGrid's v3 Node.js Library
+    // using Twilio SendGrid's v3 Node.js Library
     // https://github.com/sendgrid/sendgrid-nodejs
     const sgMail = require('@sendgrid/mail');
     sgMail.setApiKey(process.env.SENDGRID_API_KEY);
     const msg = {
-        to: 'andrew.crawford.za@gmail.com',
-        from: 'test@example.com',
-        subject: 'Sending with SendGrid is Fun',
-        text: 'and easy to do anywhere, even with Node.js',
-        html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    to: 'test@example.com',
+    from: 'test@example.com',
+    subject: 'Sending with Twilio SendGrid is Fun',
+    text: 'and easy to do anywhere, even with Node.js',
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
     };
     sgMail.send(msg);
-
-    res.send('Email sent')
 })
