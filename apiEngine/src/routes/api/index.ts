1f6164481fff7840ba0c686ac355e973ec06409a
code-in-time@hotmail.com <andrew.crawford.za@gmail.com>
2019-09-14 05:55:05 +0200

    rename route

--- apiEngine/src/routes/api/index.ts
+++ apiEngine/src/routes/api/index.ts
@@ -1,8 +1,9 @@
 var router = require('express').Router();
 
 router.use("/email", require("./email"));
-router.use("/url", require("./url"));
+router.use("/names", require("./names"));
 router.use("/save", require("./save"));
 router.use("/err", require("./testError"));
+router.use("/form", require("./form"));
 
 module.exports = router;
