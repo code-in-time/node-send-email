f034c8ef1e94cfb12034c985ee4076596ba929be
code-in-time@hotmail.com <andrew.crawford.za@gmail.com>
2019-09-15 06:57:27 +0200

    Api engine get the  id from the url

--- apiEngine/src/routes/api/form.ts
+++ apiEngine/src/routes/api/form.ts
@@ -1,23 +1,47 @@
 import express = require('express');
+import * as core from "express-serve-static-core"
 
-interface IPayload {
+interface IPostPayload {
     name: string,
     email: string,
     mobile: string
 }
 
-interface ICustomRequest<T> extends express.Request {
+interface IGetParams {
+    id: string
+}
+
+interface IPostCustomRequest<T> extends express.Request {
     body: T
 }
 
+// interface IGetCustomRequest<T> extends express.Request {
+//     params: T
+// }
+
+interface IGetCustomRequest<T extends core.ParamsDictionary> extends Request {
+    // params: T;
+    // [params: string]: T
+  }
+
 var router = require('express').Router();
 
-router.post("/", (req: ICustomRequest<IPayload>, res: express.Response) => {
+router.post("/", (req: IPostCustomRequest<IPostPayload>, res: express.Response) => {
     // res.json(["form1"]);
 
     console.log('sent', req.body)
 
     res.json(`${req.body} sent -----------------------`);
+});
+
+// TODO: https://stackoverflow.com/questions/57932422/type-t-is-not-assignable-to-type-dictionarystring
+// router.get("/:id", (req: IGetCustomRequest<IGetParams>, res: express.Response) => {
+router.get("/:id", (req: express.Request, res: express.Response) => {
+    // res.json(["form1"]);
+
+    console.log('sent', req.params)
+
+    res.json(`${req.params.id} get params -----------------------`);
 });
 
 module.exports = router; 
