82c452907341255c4ed5476c0e725f5f864a931e
code-in-time@hotmail.com <andrew.crawford.za@gmail.com>
2019-09-16 21:25:45 +0200

    api to upload a file

--- apiEngine/src/routes/api/upload.ts
+++ apiEngine/src/routes/api/upload.ts
@@ -1,36 +1,31 @@
 import express = require('express');
 
 var router = require('express').Router();
+const formidable = require('formidable')
+var fs = require('fs');
 
 
-router.post('/', async (req: any, res: any) => {
-    try {
-        if(!req.files) {
-            res.send({
-                status: false,
-                message: 'No file uploaded'
-            });
-        } else {
-            // //Use the name of the input field (i.e. "avatar") to retrieve the uploaded file
-            // let avatar = req.files.avatar;
-            
-            // //Use the mv() method to place the file in upload directory (i.e. "uploads")
-            // avatar.mv('./uploads/' + avatar.name);
 
-            //send response
-            res.send({
-                status: true,
-                message: 'File is uploaded',
-                // data: {
-                //     name: avatar.name,
-                //     mimetype: avatar.mimetype,
-                //     size: avatar.size
-                // }
-            });
-        }
-    } catch (err) {
-        res.status(500).send(err);
-    }
+router.post('', async (req: any, res: any) => {
+    new formidable.IncomingForm().parse(req)
+    .on('field', (name: any, field: any) => {
+      console.log('Field', name, field)
+    })
+    .on('file', (name: any, file: any) => {
+    //   console.log('Uploaded file', name, file)
+    console.log('Uploaded file', file.name, file.type)
+    // TODO read and save file to disk
+    })
+    .on('aborted', () => {
+      console.error('Request aborted by the user')
+    })
+    .on('error', (err: any) => {
+      console.error('Error', err)
+      throw err
+    })
+    .on('end', () => {
+      res.end()
+    })
 });
 
 module.exports = router; 
